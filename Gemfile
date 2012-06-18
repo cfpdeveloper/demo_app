66f7e2c4b053714548ca0804c1c846107480b073
cfpdeveloper <admin@crowdfundingpro.com>
2012-06-18 07:26:45 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+group :development do
+  gem 'sqlite3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f80132de5cffedba59af998da2bb62467d9f22e0cd7d1deb68f8cecc4a4327964a4c32874aaf30295f43a6db662de7a84dafa85c958670657de237ea29db9e2d'
